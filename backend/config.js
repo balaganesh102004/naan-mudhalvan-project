9c52a87255e41550a9be0f73bf99dcc23221fd45
BALAGANESH <balaganesh10.2004@gmail.com>
2024-11-15 15:17:39 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://balaganesh102004:ALQP@weQ*12@cluster0.nsgbb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://cluster0.nsgbb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     console.log("connected to mongodb");
